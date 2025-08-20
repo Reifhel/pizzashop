f35a885fdc124c20981b645dd5831cab614e17a9
Reifhel <rafaelherdt1@gmail.com>
2025-08-20 10:07:55 -0300

    feat: initial files

---
+++ web/eslint.config.js
@@ -0,0 +1,30 @@
+import js from "@eslint/js";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import { globalIgnores } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+    },
+  },
+]);
---
+++ web/src/pages/auth/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Helmet } from "react-helmet-async";
+
+export function SignIn() {
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+
+      <h1>signin</h1>
+    </>
+  );
+}
