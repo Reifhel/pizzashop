6309b93d37d9a4d564f2a07765e835cd32899c16
Reifhel <rafaelherdt1@gmail.com>
2025-08-25 10:58:46 -0300

    feat: adding the api client

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,12 @@
 import "./index.css";
 
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 
 import { Toaster } from "@/components/ui/sonner";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
+import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
 export function App() {
@@ -13,7 +15,9 @@ export function App() {
       <ThemeProvider storageKey="pizzahop-theme" defaultTheme="dark">
         <title>pizza.shop</title>
         <Toaster richColors />
-        <RouterProvider router={router} />;
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />;
+        </QueryClientProvider>
       </ThemeProvider>
     </>
   );
